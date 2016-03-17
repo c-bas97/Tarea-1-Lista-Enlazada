4666d52734b08f076ac8fff404a3f27ae686f05e
c-bas97 <sebastian2015055146@gmail.com>
2016-03-17 13:46:48 -0600

    Sin estructuras

--- ALIENS.c
+++ ALIENS.c
@@ -1,31 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct alien_t {
-	char nombre [30];
-	int id;
-	char * especie;
-	struct planetas_t * planetas;
-	struct idiomas_t * idiomas;
-	struct alien_t * siguiente;
-} typedef alien;
-
-struct planetas_t {
-	char nombre[30];
-	int starwars_code;
-	char agua;
-	char oxigeno;
-	struct planetas_t * siguiente;
-} typedef planeta;
-
-struct idiomas_t {
-	char nombre[30];
-	char tipo;
-	int simbolos;
-	struct idiomas_t * siguiente;
-	struct planetas_t * planetas;
-} typedef idioma;
+#include "estructuras.h"
 
 main() {
 	alien * lista_aliens;
