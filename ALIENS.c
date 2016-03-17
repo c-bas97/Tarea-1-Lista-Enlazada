69743953f5318dc965b7b73be7a074465cfc682e
matt96 <jkherrera96@gmail.com>
2016-03-17 14:55:42 -0600

    cambios a archivos

--- ALIENS.c
+++ ALIENS.c
@@ -2,75 +2,22 @@
 
 int main() {
 
-	alien* Aliens1=malloc (sizeof(alien));
+	alien* Aliens1 = malloc (sizeof(alien));
 	datosAlien(Aliens1);
 	
-	planeta* Planetas1=malloc (sizeof(planeta));
+	planeta* Planetas1 = malloc (sizeof(planeta));
 	datosPlaneta(Planetas1);
 	
-	idioma* Idiomas1=malloc (sizeof(idioma));
+	idioma* Idiomas1 = malloc (sizeof(idioma));
 	datosIdioma(Idiomas1);
+	
+	Aliens1 -> planetas = Planetas1;
+	Aliens1 -> idiomas = Idiomas1;
+	Idiomas1 -> planetas = Planetas1;
 
-	alien * lista_aliens;
-	lista_aliens = malloc (sizeof(alien));
-	strcpy (lista_aliens->nombre , "ET");
-	lista_aliens->id = 3;
-	lista_aliens -> especie = malloc (sizeof ( "Rarifero"));
-	strcpy (lista_aliens->especie , "Rarifero");
-		planeta * planets;
-		planets = malloc (sizeof(planeta));
-		strcpy (planets->nombre , "ETland");
-		planets->starwars_code = 23;
-		planets->agua = 'T';
-		planets->oxigeno = 'F';
-		planets->siguiente = 0;
-	lista_aliens->planetas = planets;
-		idioma * idiom;
-		idiom  = malloc(sizeof(idioma));
-		strcpy (idiom->nombre , "Etliano");
-		idiom->tipo = '4';
-		idiom->simbolos = 99;
-		idiom->siguiente = NULL;
-		idiom->planetas = planets;
-	lista_aliens->idiomas = idiom;
-	lista_aliens->siguiente = 0;
-	
-	
-	printf ("Nombre del alien: ");
-	printf ("%s\n",lista_aliens->nombre);
-	printf ("Identificación del alien: ");
-	printf ("%d\n",lista_aliens->id);
-	printf ("Especie del alien: ");
-	printf ("%s\n",lista_aliens->especie);
-	printf ("Planeta del alien: ");
-	printf ("%s\n",lista_aliens->planetas->nombre);
-	printf ("Código del pĺaneta: ");
-	printf ("%d\n",lista_aliens->planetas->starwars_code);
-	printf ("El planeta tiene agua: ");
-	printf ("%c\n",lista_aliens->planetas->agua);
-	printf ("Hay oxígeno en el planeta: ");
-	printf ("%c\n",lista_aliens->planetas->oxigeno);
-	if (lista_aliens->planetas->siguiente != NULL){
-		printf ("Siguiente planeta: ");
-		printf ("%s\n",lista_aliens->planetas->siguiente->nombre);
-	} else
-		printf ("No hay otro planeta de procedencia de este alien.");
-	printf ("Idioma del alien: ");
-	printf ("%s\n",lista_aliens->idiomas->nombre);
-	printf ("Tipo del idioma:");
-	printf ("%c\n",lista_aliens->idiomas->tipo);
-	printf ("Cantidad de símbolos del idioma: ");
-	printf ("%d\n",lista_aliens->idiomas->simbolos);
-	if (lista_aliens->idiomas->siguiente != NULL){
-		printf ("Siguiente idioma: ");
-		printf ("%s\n",lista_aliens->idiomas->siguiente->nombre);
-	} else
-		printf ("Este alien no sabe más idiomas.\n");
-	if (lista_aliens->idiomas->planetas != NULL){
-		printf ("Planeta en el que se habla el idioma: ");
-		printf ("%s\n",lista_aliens->idiomas->planetas->nombre);
-	} else
-		printf ("ESte idioma no se habla en ningún planeta conocido.\n");
+	printf ("----------------------------------------");
+
+	impresion (Aliens1);
 
 	return 0;
 }
--- funciones.c
+++ funciones.c
@@ -9,6 +9,13 @@ void datosAlien(alien* nuevo)
 		printf("\nDigite el ID del alien: ");
 		scanf("%d",&nuevo->id);
 
+		char aux[20];
+		printf("\nDigite la especie del alien: ");
+		scanf("%s",&aux[0]);
+		
+		nuevo->especie=malloc(sizeof(strlen(aux)));
+		strcpy(nuevo->especie,aux);
+		
 		nuevo->planetas=NULL;
 		nuevo->idiomas=NULL;
 		nuevo->siguiente=NULL;
@@ -23,17 +30,19 @@ void datosPlaneta(planeta* nuevo)
 	if(nuevo!=NULL)
 	{
 		printf("\nDigite el nombre del planeta: ");
-		fgets(nuevo->nombre, 30, stdin);
+		//fgets(nuevo->nombre, 30, stdin);
+		scanf("%s",&nuevo->nombre[0]);
 		
 		printf("\nDigite starwars code del planeta: ");
 		scanf("%d",&nuevo->starwars_code);
+			
+		printf("\nTiene oxigeno s/n: ");
+		//nuevo->oxigeno=getchar();
+		scanf("%c",&nuevo->oxigeno);
 		
 		printf("\nTiene agua s/n: ");
 		nuevo->agua=getchar();
-		
-		printf("\nTiene oxigeno s/n: ");
-		nuevo->oxigeno=getchar();
-		
+		//scanf("%c",&nuevo->agua);		
 		nuevo->siguiente=NULL;
 	}
 	else
@@ -46,17 +55,58 @@ void datosIdioma(idioma* nuevo)
 	if(nuevo!=NULL)
 	{
 		printf("\nDigite el nombre del idioma: ");
-		fgets(nuevo->nombre, 30, stdin);
+		//fgets(nuevo->nombre, 30, stdin);
+		scanf("%s",&nuevo->nombre[0]);
+		
+		printf("\nTipo de idioma s/n: ");
+		//nuevo->tipo=getchar();
+		scanf("%c",&nuevo->tipo);
 		
 		printf("\nDigite la cantidad de simbolos: ");
 		scanf("%d",&nuevo->simbolos);
-		
-		printf("\nTipo de idioma s/n: ");
-		nuevo->tipo=getchar();
 		
 		nuevo->siguiente=NULL;
 		nuevo->planetas=NULL;		
 	}
 	else
 		printf("\nno se pudo crear idioma");
+}
+
+void impresion ( alien * alienigena ) {
+	
+	printf ("\nNombre del alien: ");
+	printf ("%s\n",alienigena->nombre);
+	printf ("Identificación del alien: ");
+	printf ("%d\n",alienigena->id);
+	printf ("Especie del alien: ");
+	//printf ("%s\n",alienigena->especie);
+	printf ("Planeta del alien: ");
+	printf ("%s\n",alienigena->planetas->nombre);
+	printf ("Código del pĺaneta: ");
+	printf ("%d\n",alienigena->planetas->starwars_code);
+	printf ("El planeta tiene agua: ");
+	printf ("%c\n",alienigena->planetas->agua);
+	printf ("Hay oxígeno en el planeta: ");
+	printf ("%c\n",alienigena->planetas->oxigeno);
+	if (alienigena->planetas->siguiente != NULL){
+		printf ("Siguiente planeta: ");
+		printf ("%s\n",alienigena->planetas->siguiente->nombre);
+	} else
+		printf ("\nNo hay otro planeta de procedencia de este alien.");
+	printf ("\nIdioma del alien: ");
+	printf ("%s\n",alienigena->idiomas->nombre);
+	printf ("Tipo del idioma:");
+	printf ("%c\n",alienigena->idiomas->tipo);
+	printf ("Cantidad de símbolos del idioma: ");
+	printf ("%d\n",alienigena->idiomas->simbolos);
+	if (alienigena->idiomas->siguiente != NULL){
+		printf ("Siguiente idioma: ");
+		printf ("%s\n",alienigena->idiomas->siguiente->nombre);
+	} else
+		printf ("Este alien no sabe más idiomas.\n");
+	if (alienigena->idiomas->planetas != NULL){
+		printf ("Planeta en el que se habla el idioma: ");
+		printf ("%s\n",alienigena->idiomas->planetas->nombre);
+	} else
+		printf ("ESte idioma no se habla en ningún planeta conocido.\n");
 }
